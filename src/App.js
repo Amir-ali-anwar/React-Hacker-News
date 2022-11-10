2be5c5b44b6c64c1c1b3bfb6c73750599831644e
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-11-10 22:04:11 +0500

    working on reducer and context api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SearchForm from './SearchForm'
 import Stories from './Stories'
 import Buttons from './Buttons'
 function App() {
-  return <h2>hacker news starter</h2>
+  return <Stories />
 }
 
 export default App
--- src/reducer.js
+++ src/reducer.js
@@ -4,7 +4,22 @@ import {
   REMOVE_STORY,
   HANDLE_PAGE,
   HANDLE_SEARCH,
-} from './actions'
+} from "./actions";
 
-const reducer = () => {}
-export default reducer
+const reducer = (action, state) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true };
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.payload.hits,
+        nbpages: action.payload.nbpages,
+      };
+    default:
+      return state;
+    // throw new Error(`no mathching "${action.type}" action type`);
+  }
+};
+export default reducer;
